2280943806750609871103e300b6b040c507d91b
Stuart King <stuartrexking@gmail.com>
2017-09-18 16:12:41 +1000

    Corrected project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject timbre-lambda "0.0.1"
+(defproject fierce/timbre-lambda "0.0.1"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.amazonaws/aws-lambda-java-core "1.1.0"]
                  [com.taoensso/timbre "4.10.0"]]
