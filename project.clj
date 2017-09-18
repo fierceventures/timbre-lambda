9d638435c94187d22c8b3d618efe70ff4de574e2
Stuart King <stuartrexking@gmail.com>
2017-09-18 16:16:54 +1000

    Add license, description, url and author

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
 (defproject fierce/timbre-lambda "0.0.1"
+  :author "Stuart King <https://fierce.ventures>"
+  :description "timbre appender for AWS Lambda"
+  :license {:name         "Eclipse Public License"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments     "Same as Clojure"}
+  :url "https://github.com/fierceventures/timbre-lambda"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.amazonaws/aws-lambda-java-core "1.1.0"]
                  [com.taoensso/timbre "4.10.0"]]
